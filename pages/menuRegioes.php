f8f80b81475d77b5201a45462b205525534534ed
GoDllkE <gustavot53@gmail.com>
2017-10-29 14:31:10 -0200

    Alterando nome

---
+++ pages/menuRegioes.php
@@ -0,0 +1,18 @@
+<?php 
+    // Script de conexão
+    include "./AbreConexao.php";
+
+    // Query para regiões
+    $sqlRegioes="SELECT * FROM regioes";
+    $rsRegioes = mysqli_query($conn, $sqlRegioes) or die (mysqli_error($conn));
+?> 
+
+<!-- submenu dropdown da navbar localizada no index.php -->
+<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+<?php
+    while($tblRegioes=mysqli_fetch_array($rsRegioes)){ ?>
+        <a class="dropdown-item" href="./index.php?codPg=11&codReg=<?= $tblRegioes['nomeR']?>">
+            <?=$tblRegioes["nomeR"];?>
+        </a>
+    <?php } ?>
+</div>
