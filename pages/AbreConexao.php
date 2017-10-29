6b830b7c4364249fc4c41f6146d5c0bb224a2ace
GoDllkE <gustavot53@gmail.com>
2017-10-29 14:34:30 -0200

    Limpeza de codigo

--- pages/AbreConexao.php
+++ pages/AbreConexao.php
@@ -1,13 +1,12 @@
 <?php
-    // Dados de conexão
+    // [Static] Dados de conexão 
     $servidor="localhost";
     $usuario="empresa";
     $senha="123456";
     $nomeDB="bdEmpresas";
-    
-    // String de conexao
-    $conn=mysqli_connect($servidor,$usuario,$senha,$nomeDB) or die(mysqli_error($conn));
-    
-    // Definição do tipo de caracter utilizado
-    mysqli_set_charset($conn, "utf8");
+
+    // Abre conexão
+    $conn = mysqli_connect($servidor,$usuario,$senha,$nomeDB) 
+        or die(mysqli_error($conector));
+
 ?>
